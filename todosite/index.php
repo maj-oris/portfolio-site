36cf199176c75b4839736d2351a5061871180d7d
maj-oris <36729113+maj-oris@users.noreply.github.com>
2021-01-10 22:19:20 +0000

    Add files via upload

--- todosite/index.php
+++ todosite/index.php
@@ -1,6 +1,14 @@
 <?php
   session_start();
 
+  if(isset($_POST['cks_yes']) or isset($_COOKIE["tdw_notes"])){
+    $_SESSION['cookies_allowed'] = true;
+  }
+
+  if(isset($_POST['cks_no'])){
+    $_SESSION['cookies_allowed'] = false;
+  }
+
   if(isset($_POST['del_card'])){
     unset($_SESSION["cards"][$_POST['sub_id']]);
     $_SESSION["cards"] = array_values($_SESSION["cards"]);
@@ -20,15 +28,17 @@
     array_push($_SESSION["cards"], [$_POST["notename"], $_POST["notetext"]]);
   }
 
-  //if edits have been made to the notes variable, then update the cookie. if not, the site could be being reloaded or could be being loaded for the first time.
+  //if edits have been made to the notes variable, then update the cookie (if cookies are allowed). if not, the site could be being reloaded or could be being loaded for the first time.
   //in the latter case, if there is a cookie storing previous data, load that. if not, initialize the cards variable.
-  if(isset($_POST['del_card']) || isset($_POST['submit'])) {
-    setcookie("tdw_notes", serialize($_SESSION["cards"]), time() + (86400 * 30)); // 86,400 s = 1 d
-  } else {
-    if(isset($_COOKIE["tdw_notes"])){
-      $_SESSION["cards"] = unserialize($_COOKIE["tdw_notes"]);
+  if($_SESSION['cookies_allowed'] == true){
+    if(isset($_POST['del_card']) || isset($_POST['submit'])) {
+      setcookie("tdw_notes", serialize($_SESSION["cards"]), time() + (86400 * 30)); // 86,400 s = 1 d
     } else {
-      $_SESSION["cards"] = [];
+      if(isset($_COOKIE["tdw_notes"])){
+        $_SESSION["cards"] = unserialize($_COOKIE["tdw_notes"]);
+      } else {
+        $_SESSION["cards"] = [];
+      }
     }
   }
 
@@ -41,6 +51,20 @@
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
+
+    <?php
+      if(!isset($_SESSION['cookies_allowed'])){
+        //if user has not yet indicated whether they want to allow cookies or not
+        echo "<form class=\"card\" action=\"index.php\" method=\"post\">";
+        echo "<p>This website uses cookies (data saved to your browser) to enhance user experience by allowing data (chiefly notes) to be remembered between sessions.</p>";
+        echo "<br>";
+        echo "<p>Do you wish to allow cookies?</p>";
+        echo "<br>";
+        echo "<input type=\"submit\" name=\"cks_yes\" value=\"Yes\">";
+        echo "<input type=\"submit\" name=\"cks_no\" value=\"No (will not ask you again)\">";
+        echo "</form>";
+      }
+    ?>
 
     <form action="new.php">
       <input class="sitelink" type="submit" value="New Note" />
